e177409359ed81e243fb108e357f5cb75986700a
unknown <lejlahadzovic>
2021-11-15 10:46:54 +0100

    End of API paging ,filtering , sorting & searching

--- Core/Specifications/ProductWithFiltersForCountSpecification.cs
+++ Core/Specifications/ProductWithFiltersForCountSpecification.cs
@@ -9,6 +9,7 @@ namespace Core.Specifications
     public class ProductWithFiltersForCountSpecification : BaseSpecification<Product>
     {
         public ProductWithFiltersForCountSpecification(ProductSpecParams productParamas) : base(x =>
+         (string.IsNullOrEmpty(productParamas.Search) || x.Name.ToLower().Contains(productParamas.Search)) &&
          (!productParamas.BrandId.HasValue || x.ProductBrandId == productParamas.BrandId) &&
         (!productParamas.TypeId.HasValue || x.ProductTypeId == productParamas.TypeId))
         {
